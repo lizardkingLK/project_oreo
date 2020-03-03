772f41c94228cd2015e7ddf63454e95802ff479f
Sandeepa Fernando <chansanfdo1996@outlook.com>
2020-03-03 19:53:04 +0530

    sndp_6

---
+++ client/src/components/NavigationBar.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const AppNavbar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div id="navbar">
+      <Navbar color="light" light expand="sm">
+        <NavbarBrand className="txt_ternary" href="/">Oreo</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="#">Men</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Women</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Kids</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Collections</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                <i class="fas fa-user"></i>
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Sign-Up
+                </DropdownItem>
+                <DropdownItem>
+                  About
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Contact
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+            <NavbarText><NavLink href="https://github.com/lizardkingLK/project_oreo"><i class="fab fa-github col_primary_accent"></i></NavLink></NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default AppNavbar;
--- server/models/Item.js
+++ server/models/Item.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const ItemSchema = new Schema( {
     name: {
         type: String,
-        available: true
+        required: true,
+        lowercase: true
     },
     date: {
         type: Date,
