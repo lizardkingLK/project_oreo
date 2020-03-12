2a1c94617f44f30cc68244b2051fbc3613b4aeaf
Sandeepa Fernando <chansanfdo1996@outlook.com>
2020-03-12 17:01:31 +0530

    sndp_16

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const items = require('./routes/api/items');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const uri = require('./config/keys').mongoURI_A;
+let uri = require('./config/keys').mongoURI_B || process.env.mongoURI_A;
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true}, (err,db) => {
     console.log('db_oreo online...');
 });
