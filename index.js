24187768f19a092811796b5a4eca93b8d25d4b66
Sandeepa Fernando <chansanfdo1996@outlook.com>
2020-03-17 20:13:50 +0530

    sndp_25

--- index.js
+++ index.js
@@ -6,11 +6,24 @@ const app = express();
 const server = require('http').Server(app);
 const port = process.env.PORT || 3001;
 const items = require('./routes/api/items');
+let uri = '';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-let uri = require('./config/keys').mongoURI_B || process.env.MONGODB_URI;
+// serve static assests if in production
+if(process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*'), (req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    }
+
+    uri = process.env.MONGODB_URI;
+}
+
+uri = require('./config/keys').mongoURI_B;
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true}, (err,db) => {
     console.log('db_oreo online... ',uri);
 });
@@ -22,16 +35,6 @@ db.once('open', function() {
 });
 
 app.use('/api/items', items);
-
-// serve static assests if in production
-if(process.env.NODE_ENV === 'production') {
-    // set static folder
-    app.use(express.static('client/build'));
-
-    app.get('*'), (req,res) => {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    }
-}
 
 server.listen(port, () => {
     console.log('api_oreo listening on port %s', port);
