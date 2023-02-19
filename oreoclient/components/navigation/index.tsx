b272b2c0166991c0b863d0addcd45b8e626016eb
lizardkinglk <chansanfdo@gmail.com>
2023-02-19 15:43:43 +0000

    updated responsiveness

--- oreoclient/components/navigation/index.tsx
+++ oreoclient/components/navigation/index.tsx
@@ -4,29 +4,39 @@ const Navigation = () => {
   return (
     <nav className="flex flex-row bg-black p-4">
       <div className="basis-1/2">
-        <a href="/">
-          <h1 className="brand text-4xl text-white font-bold">OREO</h1>
-        </a>
+        <h1 className="brand text-4xl text-white font-bold">
+          <a href="/">
+            OREO
+          </a>
+        </h1>
       </div>
       <ul className="basis-1/2 flex flex-row align-middle justify-end">
-        <li className="mx-4">
-          <a href="#browse">
-            <h1 className="text-3xl text-white">Browse</h1>
-          </a>
+        <li className="flex justify-center content-center flex-wrap ml-4 w-10 bg-green-300 rounded-full">
+          <h1 className="text-lg text-black">
+            <a href="#browse" title="Browse Items">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+              </svg>
+            </a>
+          </h1>
         </li>
-        <li className="flex justify-center content-center flex-wrap mx-4 w-10 bg-green-300 rounded-full">
-          <a href="/cart">
-            <h1 className="text-lg text-black">
-              <i className="fa-solid fa-cart-shopping"></i>
-            </h1>
-          </a>
+        <li className="flex justify-center content-center flex-wrap ml-4 w-10 bg-green-300 rounded-full">
+          <h1 className="text-lg text-black">
+            <a href="/cart" title="Checkout Items">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+              </svg>
+            </a>
+          </h1>
         </li>
-        <li className="flex justify-center content-center flex-wrap mx-4 w-10 bg-green-500 rounded-full">
-          <a href="/profile">
-            <h1 className="text-lg text-white">
-              <i className="fas fa-user"></i>
-            </h1>
-          </a>
+        <li className="flex justify-center content-center flex-wrap ml-4 w-10 bg-green-300 rounded-full">
+          <h1 className="text-lg text-black">
+            <a href="/profile" title="Edit Profile">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+              </svg>
+            </a>
+          </h1>
         </li>
       </ul>
     </nav>
--- oreoclient/pages/index.tsx
+++ oreoclient/pages/index.tsx
@@ -7,27 +7,19 @@ export default function Home() {
     <Layout>
       <main className="bg-black p-2">
         <div className="flex py-14 pb-32">
-          <div className="basis-2/4 bg-gradient-to-r from-green-500 to-green-300">
-            <h1 className="text-9xl font-black ">
+          <div className="basis-full md:basis-2/4 bg-gradient-to-r from-green-500 to-green-300">
+            <h1 className="text-5xl md:text-9xl font-black ">
               One of the Shopping Stores.
             </h1>
             <blockquote className="mx-2 font-bold">
               Browse, Buy, Deliver T-Shirts.
             </blockquote>
           </div>
-          <div className="basis-2/4 border-2 flex justify-center content-center">
-            <Image
-              width={387}
-              height={387}
-              src={"/logo.png"}
-              alt={"hero logo"}
-            />
-          </div>
         </div>
-        <div className="pt-8 pb-16" id="browse">
+        <div className="pt-8 pb-24" id="browse">
           <h1 className="text-5xl text-white"># Browse</h1>
           <div className="flex justify-center">
-            <div className="pt-16 grid grid-cols-4 xs:grid-cols-2 gap-4 justify-center">
+            <div className="pt-16 grid grid-cols-2 md:grid-cols-4 gap-4 justify-center">
               {Array(7)
                 .fill(1)
                 .map((v, i) => (
@@ -41,16 +33,6 @@ export default function Home() {
                     />
                   </a>
                 ))}
-              <div className="flex flex-wrap justify-center content-center border-2 hover:border-green-500">
-                <a href="/design/create">
-                  <h1
-                    className="text-8xl font-black text-white hover:text-green-500"
-                    title="Add More"
-                  >
-                    <i className="fa-solid fa-plus"></i>
-                  </h1>
-                </a>
-              </div>
             </div>
           </div>
         </div>
