e87d2557b25e5764e20ea9eb855f242d98a41d59
lizardkingLK <chansanfdo@gmail.com>
2023-04-14 21:04:08 +0530

    repositioned is typing further down.

--- components/cards/message/index.tsx
+++ components/cards/message/index.tsx
@@ -5,27 +5,24 @@ import { IMessageCardProps } from "@/types";
 
 export default function MessageCard(props: IMessageCardProps) {
   if (props) {
-    const parentContainerClass = `flex ${props.type === messageTypes.SENT && 'justify-end'}`;
-    const containerClass = `p-4 m-4 min-w-max rounded-xl bg-gradient-to-r 
-    ${props.type === messageTypes.RECEIVED
-        ? 'from-green-400 to-green-500 rounded-tl-none cursor-pointer'
-        : 'from-orange-400 to-orange-500 rounded-tr-none cursor-pointer'}`;
-    const contentClass = `max-w-xs text-md text-black font-bold ${props.type === messageTypes.SENT && 'text-right'}`;
-
+    const { type, messageAuthorName, messageTime, messageImagePath } = props;
     return (
-      <div className={parentContainerClass}>
-        {props.type === messageTypes.RECEIVED && props.messageAuthorName && props.messageTime && props.messageImagePath
+      <div className={`flex ${type === messageTypes.SENT && 'justify-end'}`}>
+        {messageAuthorName && messageTime && messageImagePath && type === messageTypes.RECEIVED
           ? <div className='hidden md:flex flex-col justify-start items-center mt-4'>
             <Avatar
-              name={props.messageAuthorName}
-              imagePath={props.messageImagePath}
+              name={`${messageAuthorName} : ${messageTime}`}
+              imagePath={messageImagePath}
               size={60}
             />
-            <p className='text-sm text-white ml-2'>{props.messageTime}</p>
           </div>
           : null}
-        <div className={containerClass}>
-          <h1 className={contentClass}>
+        <div className={`p-4 m-4 min-w-max rounded-xl bg-gradient-to-r 
+        ${type === messageTypes.RECEIVED
+            ? 'from-green-400 to-green-500 rounded-tl-none cursor-pointer'
+            : 'from-orange-400 to-orange-500 rounded-tr-none cursor-pointer'}`}>
+          <h1 className={`max-w-xs text-md text-black font-bold 
+          ${type === messageTypes.SENT ? 'text-right' : ''}`}>
             {props.content}
           </h1>
         </div>
