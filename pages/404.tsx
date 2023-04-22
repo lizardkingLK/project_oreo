168da622bce81d4cb029212c344b0c06a3ad4185
lizardkingLK <chansanfdo@gmail.com>
2023-04-23 00:05:13 +0530

    updated 404 page. made editor responsive

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,24 +4,17 @@ import React from 'react'
 
 const Error = () => {
     return (
-        <>
-            <div className="m-20 text-center">
-                <Image
-                    width={150}
-                    height={150}
-                    src={"/logo.png"}
-                    alt={"hero logo"}
-                />
-            </div>
-            <h1 className='m-20 text-5xl md:text-8xl text-white font-black'>
+        <div className='flex flex-col justify-center items-center h-screen'>
+            <Image className='m-2' width={50} height={50} alt='logo' src={"/favicon.png"} />
+            <h1 className='m-2 text-4xl text-white font-bold'>
                 Page Not Found
             </h1>
-            <h1 className='m-20 text-5xl text-white font-black underline'>
+            <h1 className='m-2 text-xl text-green-500 font-bold'>
                 <Link href='/'>
-                    Back to Home
+                    Home
                 </Link>
             </h1>
-        </>
+        </div>
     )
 }
 
