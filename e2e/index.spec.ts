fc58538e3b4775a983667234f881ea60e78915b5
lizardkingLK <chansanfdo@gmail.com>
2023-08-09 16:34:04 +0530

    updated e2e tests

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -7,29 +7,4 @@ test("should welcome by the index page", async ({ page }) => {
 
   // The page should contain an h1 with "OREO"
   await expect(page.locator("h1")).toContainText("OREO");
-
-  // The page should contain navbar toggle
-  await expect(page.locator("button#btnToggleNavbar")).toBeVisible();
-});
-
-test("should login to the system", async ({ page }) => {
-  await page.goto("/");
-
-  await page.click("button#btnToggleNavbar");
-
-  await page.getByText("Login").click();
-
-  await page.getByText("Sign in with Credentials").click();
-
-  await expect(page).toHaveTitle("Sign In");
-
-  await page.getByLabel("Email").fill("john@gmail.com");
-
-  await page.getByLabel("Password").fill("john");
-
-  await page.getByText("Sign in with Credentials").click();
-
-  await expect(page).toHaveTitle("Oreo");
-
-  await expect(page.locator("#textGreeting")).toContainText("Hello John Doe");
 });
