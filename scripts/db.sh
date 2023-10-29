761dae188c74cfc28b79ad1e27433e4003a5e6c1
lizardkingLK <chansanfdo@gmail.com>
2023-10-29 05:59:59 +0000

    updated db.sh file

---
+++ scripts/db.sh
@@ -0,0 +1,12 @@
+# db.ps1 - Run As Priviledged
+cd ../
+
+# installing dependencies
+echo "Installing Dependencies..."
+npm install -g dotenv-cli
+npm i
+
+# migrating database
+echo "Migrating Database..."
+npx prisma generate
+dotenv -e .env.local -- npx prisma migrate dev --name init
